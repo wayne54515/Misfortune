f059386bc3d1cc9a51464bce37f7896469cbd7c8
wayne54515 <0920005wayne@gmail.com>
2020-05-10 15:30:53 +0800

    Initial Unity Project
    腳色的前後左右移動
    腳色的跳躍
    腳色的持續移動並逐漸加速
    腳色有速度上限(避免地板生成跟不上)
    地板的自動生成與銷毀

---
+++ Assets/Script/GroundCreate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCreate : MonoBehaviour
+{
+    public GameObject ground;
+    private GameObject _ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            int createpos = (int)ground.transform.localPosition.z;
+            //Debug.Log(createpos);
+            _ground = Instantiate(ground, new Vector3(0, 0, createpos + 10), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Script/PlayerAction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public Vector3 jump;
+    public float jumpForce = 1.0f;
+    private bool isGrounded;
+    private Rigidbody rb;
+    private float speedUp = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * speedUp);
+        if(speedUp < 7.8f)
+            speedUp += 0.0001f;
+        //Debug.Log(speedUp);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * (speed-2) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * (speed-1) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (isGrounded && (Input.GetKey(KeyCode.Z)))
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
