f900cc9e8cdd31412e7ed8dd55bff1f68c1610b2
wayne54515 <0920005wayne@gmail.com>
2020-05-18 18:53:06 +0800

    Debug bug
    下調速度上限
    取消加速減速
    跳躍改用動畫方式實作
    新增快速下降
    地板4障礙物外表修改

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -4,54 +4,44 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour
 {
+    public Animation _anim;
     public float speed = 3.0f;
-    public Vector3 jump;
-    public float jumpForce = 1.0f;
-    private bool isGrounded;
-    private Rigidbody rb;
     private float speedUp = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime * speedUp);
-        if(speedUp < 7.8f)
-            speedUp += 0.0001f;
+        if(speedUp < 5.5f)
+            speedUp += 0.0005f;
         //Debug.Log(speedUp);
 
+        //jump
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(Vector3.forward * (speed-2) * Time.deltaTime);
+            _anim.Play("jump");
         }
+        //quick jump down
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(Vector3.back * (speed-1) * Time.deltaTime);
+            _anim.Play("down");
         }
+        //go right
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
+        //go left
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
 
-        if (isGrounded && (Input.GetKey(KeyCode.Z)))
-        {
-            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
-        }
-
-    }
-    private void OnCollisionStay(Collision collision)
-    {
-        isGrounded = true;
     }
 
     private void OnCollisionEnter(Collision collision)
