ef722b729f706199531dce2b7041443fb59ecb1b
wayne54515 <0920005wayne@gmail.com>
2020-05-21 03:09:07 +0800

    Trap auto destory
    update README

--- Assets/Script/TrapEffect.cs
+++ Assets/Script/TrapEffect.cs
@@ -6,18 +6,22 @@ public class TrapEffect : MonoBehaviour
 {
     private GameObject player,playerMain;
     private Rigidbody rb;
+    private float trapZ;
     // Start is called before the first frame update
     void Start()
     {
         playerMain = FindObjectOfType<PlayerAction>().gameObject;
         player = FindObjectOfType<PlayerAction>().gameObject.transform.GetChild(0).gameObject;
         rb = player.GetComponent<Rigidbody>();
+        trapZ = gameObject.transform.localPosition.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(trapZ);
+        if ((trapZ - playerMain.transform.localPosition.z) < -3)
+            Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
